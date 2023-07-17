6793b55f7d6e4685012aab1ffacdc6ff56074994
Puk06 <86549420+puk06@users.noreply.github.com>
2023-07-17 21:54:33 +0900

    Updated

--- Modupdater.js
+++ Modupdater.js
@@ -2,30 +2,44 @@ const fs = require('fs');
 const https = require('https');
 const axios = require('axios');
 const path = require('path');
+require('dotenv').config();
+
+const githubclientid = process.env.GITHUB_CLIENT_ID;
+const githubclientsecret = process.env.GITHUB_CLIENT_SECRET;
 
 const wannaupdatemod = [];
 fs.readdirSync("./アップデートしたいMOD").forEach(file => {
     wannaupdatemod.push(path.basename(file))
 })
-console.log(wannaupdatemod)
+
 const rawjsondata = fs.readFileSync('UpdateLinks.json', 'utf-8')
 const jsondata = JSON.parse(rawjsondata);
+
 console.log("ダウンロード中です")
-console.log("0%")
-let  percent = 0;
-for(const element of jsondata) {
-    console.log(Math.round(100 / jsondata.length * percent + 10) + "%");
-    const owner = element.link.split('/')[0];
-    const repo = element.link.split('/')[1];
-    downloadFile(owner, repo)
-    percent++;
+for(const element of wannaupdatemod) {
+    for(const json of jsondata) {
+        if (!element.includes(json.name)) {
+            continue;
+        } else {
+            console.log(`${json.name}をアップデート中です。`)
+            const owner = json.link.split('/')[0];
+            const repo = json.link.split('/')[1];
+            downloadFile(owner, repo);
+        }
+    }
 }
-console.log("100%")
-console.log("ダウンロードが完了しました")
+
+if (fs.readdirSync("./アップデート後").length == 0) {
+    console.log("アップデート出来るMODが有りませんでした")
+} else if (!fs.readdirSync("./アップデート後").length == fs.readdirSync("./アップデートしたいMOD").length) {
+    console.log(`アップデートは完了しましたが、アップデート出来なかったMODが有ります(${fs.readdirSync("./アップデートしたいMOD").length - fs.readdirSync("./アップデート後").length}個)`)
+} else {
+    console.log(`全てのアップデートが完了しました(${fs.readdirSync("./アップデート後").length}個)`)
+}
 
 async function downloadFile(owner, repo) {
     const url = await getLatestReleaseFiles(owner, repo);
-    const fileurl = url.filter(file => file.endsWith('.jar'))[0].toString();
+    const fileurl = `${url.filter(file => file.endsWith('.jar'))[0].toString()} + "?client_id=${githubclientid}&client_secret=${githubclientsecret}"`;
     const dest = `./アップデート後/${url[0].split('/')[url[0].split('/').length - 1]}`;
     const res = await axios.get(fileurl, {responseType: 'arraybuffer'});
     fs.writeFileSync(dest, res.data, 'UTF-8');
